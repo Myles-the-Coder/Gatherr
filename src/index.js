023c171e0611c27e69b7039317cef84afeb939a5
Myles-the-Coder <76969330+Myles-the-Coder@users.noreply.github.com>
2021-12-06 15:14:29 -0700

    Fixed Atatus error

--- src/index.js
+++ src/index.js
@@ -25,5 +25,3 @@ serviceWorkerRegistration.unregister();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-atatus.notify(new Error('Test Atatus Setup '));
-atatus.notify(new Error('Test Atatus Setup 2'));
