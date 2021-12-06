a7cb8e675fc5075212f10caa29bce39b6f3ecba3
Myles-the-Coder <76969330+Myles-the-Coder@users.noreply.github.com>
2021-12-06 14:47:19 -0700

    Installed Atatus and dotenv

--- src/index.js
+++ src/index.js
@@ -3,13 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import * as atatus from 'atatus-spa';
+import dotenv from 'dotenv';
 import reportWebVitals from './reportWebVitals';
+atatus.config('777e286f57f54709bd1ddc4f40074754').install();
+
+dotenv.config();
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
@@ -21,3 +26,4 @@ serviceWorkerRegistration.unregister();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+atatus.notify(new Error('Test Atatus Setup'));
