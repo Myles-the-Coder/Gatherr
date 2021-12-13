ebcfc0fc679f1d5adb896713eb95b5c1be86f6c9
Myles-the-Coder <76969330+Myles-the-Coder@users.noreply.github.com>
2021-12-13 15:42:56 -0700

    Fixed useEffect issue

--- src/components/EventGenre.js
+++ src/components/EventGenre.js
@@ -1,22 +1,24 @@
 import React, { useEffect, useState } from 'react';
-import { PieChart, Pie, ResponsiveContainer } from 'recharts';
+import { PieChart, Pie, Cell, Legend, ResponsiveContainer } from 'recharts';
 
 const EventGenre = ({ events }) => {
 	const [data, setData] = useState([]);
+
 	useEffect(() => {
+		const getData = () => {
+			const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'AngularJS'];
+			const data = genres.map(genre => {
+				const value = events.filter(event =>
+					event.summary.split(' ').includes(genre)
+				).length;
+				return { name: genre, value };
+			});
+			return data;
+		};
 		setData(() => getData());
 	}, [events]);
 
-	const getData = () => {
-		const genres = ['React', 'Javascript', 'Node', 'jQuery', 'AngularJS'];
-		const data = genres.map(genre => {
-			const value = events.filter(event =>
-				event.summary.split(' ').includes(genre)
-			).length;
-			return { name: genre, value };
-		});
-		return data;
-	};
+	const colors = ['#ff9900', '#1ae7ff', '#c90076', '#2d7566', '#fed96d'];
 
 	return (
 		<ResponsiveContainer height={400}>
@@ -28,9 +30,19 @@ const EventGenre = ({ events }) => {
 					labelLine={false}
 					outerRadius={80}
 					fill='#8884d8'
+					dataKey='value'
 					label={({ name, percent }) =>
 						`${name} ${(percent * 100).toFixed(0)}%`
-					}
+					}>
+					{data.map((entry, index) => (
+						<Cell key={`cell-${index}`} fill={colors[index % colors.length]} />
+					))}
+				</Pie>
+				<Legend
+					layout='horizontal'
+					verticalAlign='top'
+					align='center'
+					height={45}
 				/>
 			</PieChart>
 		</ResponsiveContainer>
